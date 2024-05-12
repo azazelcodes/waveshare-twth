25d0adc019a84583dc6f5d84a7fe51c826424016
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2024-05-12 15:20:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='waveshare_two_thirteen',
+    name='waveshare-twth',
     version='0.0.1',
     description='Communicate with the Waveshare 2.13" V3 with ease.',
     long_description=long_description,
