36fb0a8c7131119a0a8099207cace5b5dd37190f
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2024-05-12 15:08:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
-import sys, os
-from setuptools import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-dependencies = ['Pillow']
+here = path.abspath(path.dirname(__file__))
 
-if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
-    dependencies += ['RPi.GPIO', 'spidev']
-elif os.path.exists('/sys/bus/platform/drivers/gpio-x3'):
-    dependencies += ['Hobot.GPIO', 'spidev']
-else:
-    dependencies += ['Jetson.GPIO']
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='waveshare-epd',
-    description='Waveshare e-Paper Display',
-    author='Waveshare',
-    package_dir={'': 'lib'},
-    packages=['waveshare_epd'],
-    install_requires=dependencies,
+    name='waveshare_two_thirteen',
+    version='0.0.1',
+    description='Communicate with the Waveshare 2.13" V3 with ease.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/azazelcodes/waveshare_two_thirteen',
+    author='Azazel',
+    author_email='azazel.codes@gmail.com',
+    keywords='waveshare ',
+    #py_modules=["pyky040"],
+    packages=find_packages(),
+    install_requires=['RPi.GPIO', 'spidev'],
 )
-
