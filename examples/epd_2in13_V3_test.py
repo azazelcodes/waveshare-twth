c52ffe0bf63c16a42cdbe0945306b49e87eed893
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2024-05-12 16:11:17 +0200

    Update epd_2in13_V3_test.py

--- examples/epd_2in13_V3_test.py
+++ examples/epd_2in13_V3_test.py
@@ -1,35 +1,17 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-import sys
-import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
-import logging
-import waveshare_two_thirteen
+import wavesharetwth
 import time
 from PIL import Image,ImageDraw,ImageFont
-import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
 try:
-    logging.info("epd2in13_V3 Demo")
-    
-    epd = waveshare_two_thirteen.EPD()
-    logging.info("init and Clear")
+    epd = wavesharetwth.EPD()
     epd.init()
     epd.Clear(0xFF)
 
     # Drawing on the image
     font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    
-    logging.info("1.Drawing on the image...")
-    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
-    draw = ImageDraw.Draw(image)
     
     draw.rectangle([(0,0),(50,50)],outline = 0)
     draw.rectangle([(55,0),(100,50)],fill = 0)
@@ -47,21 +29,6 @@ try:
     epd.display(epd.getbuffer(image))
     time.sleep(2)
     
-    # read bmp file 
-    logging.info("2.read bmp file...")
-    image = Image.open(os.path.join(picdir, '2in13.bmp'))
-    epd.display(epd.getbuffer(image))
-    time.sleep(2)
-    
-    # read bmp file on window
-    logging.info("3.read bmp file on window...")
-    # epd.Clear(0xFF)
-    image1 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    image1.paste(bmp, (2,2))    
-    epd.display(epd.getbuffer(image1))
-    time.sleep(2)
-    
     # # partial update
     logging.info("4.show time...")
     time_image = Image.new('1', (epd.height, epd.width), 255)
@@ -76,18 +43,15 @@ try:
         num = num + 1
         if(num == 10):
             break
-    
-    logging.info("Clear...")
+
     epd.init()
     epd.Clear(0xFF)
-    
-    logging.info("Goto Sleep...")
+
     epd.sleep()
         
 except IOError as e:
-    logging.info(e)
+    print(e)
     
 except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd2in13_V3.epdconfig.module_exit(cleanup=True)
+    epdconfig.module_exit(cleanup=True)
     exit()
