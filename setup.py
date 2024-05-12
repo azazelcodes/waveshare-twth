1839d350489ec45877db8c7b5d5747b18e63951c
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2024-05-12 17:00:41 +0200

    IDK BROIDK ANYMORE FCK THIZ

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
+import sys, os
+from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
+dependencies = ['Pillow']
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
+    dependencies += ['RPi.GPIO', 'spidev']
+elif os.path.exists('/sys/bus/platform/drivers/gpio-x3'):
+    dependencies += ['Hobot.GPIO', 'spidev']
+else:
+    dependencies += ['Jetson.GPIO']
 
 setup(
-    name='wavesharetwth',
-    version='0.0.2',
-    description='Use the Waveshare 2.13" V3 easily.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/azazelcodes/wavesharetwth',
-    author='Raphael Yancey',
-    author_email='azazel.codes@gmail.com',
-    keywords='waveshare epaper display 2.13"',
-    #py_modules=["wavesharetwth"],
-    packages=find_packages(),
-    install_requires=['RPi.GPIO', 'spidev'],
+    name='waveshare-epd',
+    description='Waveshare e-Paper Display',
+    author='Waveshare',
+    package_dir={'': 'lib'},
+    packages=['waveshare_epd'],
+    install_requires=dependencies,
 )
+
